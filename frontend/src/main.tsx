47031fdacf63172e2321938eea307a368cffec66
kely-rufino <kelyrufino@icloud.com>
2026-06-13 09:51:37 +0100

    Adding Sentry to the project

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -6,6 +6,28 @@ import { routeTree } from './routeTree'
 import { ToastProvider } from './lib/toast'
 import './index.css'
 
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: "https://ccd0da432c944715009221648591e483@o4511557187272704.ingest.de.sentry.io/4511557194350672",
+  // To disable sending user data, uncomment the line below. For more info visit:
+  // https://docs.sentry.io/platforms/javascript/guides/react/configuration/options/#dataCollection
+  // dataCollection: { userInfo: false },
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.replayIntegration()
+  ],
+  // Tracing
+  tracesSampleRate: 1.0, //  Capture 100% of the transactions
+  // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+  tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+  // Session Replay
+  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.,
+  // Enable logs to be sent to Sentry
+  enableLogs: true
+});
+
 const queryClient = new QueryClient()
 
 const router = createRouter({
